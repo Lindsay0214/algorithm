c8e9229a72beacca6a027c711e141187542106f2
Lindsay <lliny0214tw@icloud.com>
2023-11-23 00:18:10 +0800

    20231122: Sliding Window

---
+++ 演算法/SlidingWindow/index.js
@@ -0,0 +1,38 @@
+// Max and Min Sum
+// write two function that calculate the max and min sum of n consecutive numbers in an array.
+// minSum([2, 7, 3, 0, 6, 1, -5, -12, -11], 3) -> -28
+
+// solution
+function maxSum(arr, size) {
+    let max_value = -Infinity
+
+    if (size > arr.length) return null
+
+    for (let i = 0; i <= arr.length - size; i++) {
+        let attemp = 0
+        for (let j  = i; j < i + size; j++) {
+            attemp += arr[j]
+        }
+        if (attemp > max_value) max_value = attemp
+    }
+    return max_value
+}
+maxSum([2, 7, 3, 0, 6, 1, -5, -12, -11], 3) // 12
+
+// better solution
+function maxSumSecond(arr, size) {
+    if (size < arr.length) return null
+
+    let maxValue = 0
+    for (let i = 0; i < size; i++) {
+        maxValue += arr[i]
+    }
+
+    let temValue = maxValue
+    for (let j = size; j < arr.length; j++) {
+        temValue = temValue + arr[j] - arr[j - size]
+        if (temValue > maxValue) maxValue = temValue
+    }
+    return maxValue
+}
+maxSumSecond([2, 7, 3, 0, 6, 1, -5, -12, -11], 3) // 12
---
+++ 演算法/SlidingWindow/intro.md
@@ -0,0 +1,11 @@
+Sliding Window
+- This is a well-known algorithm
+- Generally speaking, a sliding window is a sub-list that runs over an underlying collection.
+  For example, if you have an array like: `[a, b, c, d, e, f, g, h]`,
+  Than, a sliding window of size 3 would run over it like...
+```
+  [a, b, c]
+     [b, c, d]
+        [c, d, e] ...
+```
+
